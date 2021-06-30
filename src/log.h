9f87abb401fd973f4acf79fd7ec8ca31582ecb87
soyan <soyan1999@qq.com>
2021-06-30 18:45:58 +0800

    persist api

---
+++ src/log.h
@@ -0,0 +1,330 @@
+# ifndef _LOG_H_
+# define _LOG_H_
+
+# include "stm_internal.h"
+# define V_LOG_LENGTH 15
+
+# define NV_LOG_NUM 63
+# define TYPE_NV_LOG_BLOCK 1
+
+# define NV_LOG_BLOCK_NUM 1024
+# define BEGIN_SIG 0xffffffffffffffff
+# define END_SIG 0xfffffffffffffffe
+
+# define LAYOUT_NAME "dudetm"
+# define POOL_SIZE 1 * 1024 * 1024 * 1024
+
+
+typedef uint64_t nv_ptr;
+
+struct root {
+    nv_ptr obj_root[127];
+    uint64_t root_num;
+
+    nv_ptr persist_block;
+    nv_ptr reproduce_block;
+    uint64_t persist_timestamp;
+    uint64_t reproduce_timestamp;
+};
+
+typedef struct nv_log_entry {
+    nv_ptr nv_addr;
+    uint64_t data;
+} nv_log_entry_t;
+
+
+struct nv_log_block {
+    nv_ptr next;
+    uint64_t reserved;
+    nv_log_entry_t logs[NV_LOG_NUM];
+};
+
+
+TOID_DECLARE_ROOT(struct root);
+TOID_DECLARE(struct nv_log_block, TYPE_NV_LOG_BLOCK);
+
+void pmem_init(char *pool_path, void (*obj_init) ()); // use to init pmem area
+
+struct nv_log {
+    nv_ptr write_block;
+    nv_ptr read_block;
+    uint64_t write_offset;
+    uint64_t read_offset;
+
+};
+
+typedef struct v_log_entry {
+    uint64_t nv_addr;
+    uint64_t data;
+} v_log_entry_t;
+
+struct v_log_head {
+    uint64_t num;                       // only vaild in first entry
+    struct v_log_head *next;
+    v_log_entry_t v_logs[V_LOG_LENGTH];
+};
+
+
+typedef struct nv_log_begin {
+    uint64_t begin_flag;
+    uint64_t length;
+} nv_log_begin_t;
+
+typedef struct nv_log_end {
+    uint64_t end_flag;
+    uint64_t time_commit;
+} nv_log_end_t;
+
+
+void v_log_insert(stm_tx_t *tx, uint64_t nv_addr, uint64_t data); // use when write data to a new addr
+
+void v_log_insert_exist(stm_tx_t *tx, uint64_t nv_addr, uint64_t data, uint64_t nb); //use when write data to a exist addr
+
+void v_log_init(stm_tx_t *tx); // use when init tx thread
+
+void v_log_reset(stm_tx_t *tx); // use when exit tx 
+
+void nv_log_init(); // use when init stm
+
+int nv_log_record(stm_tx_t *tx, uint64_t commit_timestamp); // use when commit
+
+int nv_log_reproduce(); // use after commit
+
+void nv_log_save(); // save all log to nv_heap
+
+
+static void v_log_expand(stm_tx_t *tx) {
+    v_log_head_t *node = tx->addition.v_log_head;
+
+    while (node != NULL) {
+        node = node->next;
+    }
+
+    node = (v_log_head_t *)malloc(sizeof(v_log_head_t));
+    node->num = 0;
+    node->next = NULL;
+}
+
+void v_log_init(stm_tx_t *tx) {
+    v_log_expand(tx);
+}
+
+void v_log_insert_exist(stm_tx_t *tx, uint64_t nv_addr, uint64_t data, uint64_t nb) {
+    v_log_head_t *node = tx->addition.v_log_head;
+
+    while (nb > V_LOG_LENGTH - 1) {
+        nb -= V_LOG_LENGTH;
+        node = node->next;
+    }
+
+    node->v_logs[nb].nv_addr = nv_addr;
+    node->v_logs[nb].data = data;
+}
+
+void v_log_insert(stm_tx_t *tx, uint64_t nv_addr, uint64_t data) {
+    v_log_head_t *node = tx->addition.v_log_head;
+    unsigned int nb = node->num;
+
+    while (nb > V_LOG_LENGTH - 1) {
+        nb -= V_LOG_LENGTH;
+        if (node ->next == NULL) v_log_expand(tx);
+        node = node->next;
+    }
+
+    node->v_logs[nb].nv_addr = nv_addr;
+    node->v_logs[nb].data = data;
+    tx->addition.v_log_head->num ++;
+}
+
+void v_log_reset(stm_tx_t *tx) {
+    tx->addition.v_log_head->num = 0;
+}
+
+// persist log operation
+
+static void nv_log_alloc() {
+    PMEMoid Temp, Next;
+    struct nv_log_block *temp, *next;
+    TX_BEGIN(_tinystm.addition.pool) {
+        pmemobj_tx_add_range_direct(&_tinystm.addition.root->persist_block, 2 * sizeof(nv_ptr));
+        Temp = pmemobj_tx_alloc(sizeof(struct nv_log_block), TYPE_NV_LOG_BLOCK);
+        temp = pmemobj_direct(Temp);
+        _tinystm.addition.root->persist_block = Temp.off;
+        _tinystm.addition.root->reproduce_block = Temp.off;
+
+        for (int i = 0; i < NV_LOG_BLOCK_NUM - 1; i++) {
+            Next = pmemobj_tx_alloc(sizeof(struct nv_log_block), TYPE_NV_LOG_BLOCK);
+            next = pmemobj_direct(Next);
+
+            temp->next = Next.off;
+            Temp = Next;
+            temp = next;
+        }
+        temp->next = _tinystm.addition.root->persist_block;
+
+    }TX_END
+}
+
+static void nv_log_get(v_log_entry_t *entry) {
+    struct nv_log_block *temp;
+    temp = (struct nv_log_block *)(_tinystm.addition.nv_log->read_block + _tinystm.addition.base);
+
+    entry->nv_addr = temp->logs[_tinystm.addition.nv_log->read_offset].nv_addr;
+    entry->data = temp->logs[_tinystm.addition.nv_log->read_offset++].data;
+
+    if (_tinystm.addition.nv_log->read_offset == NV_LOG_NUM) {
+        _tinystm.addition.nv_log->read_block = temp->next;
+        _tinystm.addition.nv_log->read_offset = 0;
+    }
+}
+
+static int nv_log_insert(uint64_t *entry) {
+    struct nv_log_block *temp;
+    temp = (struct nv_log_block *)(_tinystm.addition.nv_log->write_block + _tinystm.addition.base);
+
+    temp->logs[_tinystm.addition.nv_log->write_offset].nv_addr = *entry;
+    temp->logs[_tinystm.addition.nv_log->write_offset++].data = *(entry + 1);
+
+    if (_tinystm.addition.nv_log->write_offset == NV_LOG_NUM) {
+        if (temp->next == _tinystm.addition.nv_log->read_block) return -1;
+        pmemobj_flush(_tinystm.addition.pool, (void *)_tinystm.addition.nv_log->write_block, sizeof(struct nv_log_block)); // flush
+        _tinystm.addition.nv_log->write_block = temp->next;
+        _tinystm.addition.nv_log->write_offset = 0;
+        return 0;
+    }
+    return 0;
+}
+
+static void nv_log_recovery() {
+    while (_tinystm.addition.root->persist_timestamp > _tinystm.addition.root->reproduce_timestamp) {
+        nv_log_reproduce();
+    }
+}
+
+void nv_log_init() {
+    if (_tinystm.addition.root->persist_block == 0) {
+        nv_log_alloc();
+        _tinystm.addition.nv_log->read_block = _tinystm.addition.root->persist_block;
+        _tinystm.addition.nv_log->write_block = _tinystm.addition.root->reproduce_block;
+    }
+    else {
+        _tinystm.addition.nv_log->read_block = _tinystm.addition.root->reproduce_block;
+        _tinystm.addition.nv_log->write_block = _tinystm.addition.root->persist_block;
+        _tinystm.addition.nv_log->read_offset = _tinystm.addition.root->reproduce_timestamp;
+        _tinystm.addition.nv_log->write_offset = _tinystm.addition.root->persist_timestamp;
+        nv_log_recovery();
+    }
+}
+
+int nv_log_record(stm_tx_t *tx, uint64_t commit_timestamp) {
+    nv_log_begin_t begin_block = {.begin_flag = BEGIN_SIG, .length = tx->addition.v_log_head->num};
+    nv_log_end_t end_block = {.end_flag = END_SIG, .time_commit = commit_timestamp};
+    uint64_t write_offset = _tinystm.addition.nv_log->write_offset;
+    uint64_t write_block = _tinystm.addition.nv_log->write_block;
+    v_log_head_t  *v_log = tx->addition.v_log_head;
+    int result = 0;
+    
+    result = nv_log_insert((uint64_t *)&begin_block);
+    if (result != 0) {
+        _tinystm.addition.nv_log->write_offset = write_offset;
+        _tinystm.addition.nv_log->write_block = write_block;
+        return result;
+    }
+    
+
+    for (int record_num = 0; record_num < tx->addition.v_log_head->num; record_num++) {
+        result = nv_log_insert((uint64_t *)(&(v_log->v_logs[record_num % V_LOG_LENGTH])));
+        if (result != 0) {
+            _tinystm.addition.nv_log->write_offset = write_offset;
+            _tinystm.addition.nv_log->write_block = write_block;
+            return result;
+        }
+        if (record_num % V_LOG_LENGTH == 0) v_log = v_log->next;
+    }
+
+    result = nv_log_insert((uint64_t *)&end_block);
+    if (result != 0) {
+        _tinystm.addition.nv_log->write_offset = write_offset;
+        _tinystm.addition.nv_log->write_block = write_block;
+        return result;
+    }
+
+    if (_tinystm.addition.nv_log->write_offset != 0) 
+        pmemobj_flush(_tinystm.addition.pool, (void *)_tinystm.addition.nv_log->write_block, sizeof(struct nv_log_block)); // flush
+    
+    pmemobj_drain(_tinystm.addition.pool);
+    
+    // persist log inf in root
+    struct pobj_action act[2];
+    pmemobj_set_value(_tinystm.addition.pool, &act[0], &_tinystm.addition.root->persist_block, _tinystm.addition.nv_log->write_block);
+    pmemobj_set_value(_tinystm.addition.pool, &act[1], &_tinystm.addition.root->persist_timestamp, commit_timestamp);
+    pmemobj_publish(_tinystm.addition.pool, act, 2);
+
+    tx->addition.v_log_head->num = 0; // delete v_log
+    return 0;
+}
+
+int nv_log_reproduce() {
+    if (_tinystm.addition.root->persist_timestamp == _tinystm.addition.root->reproduce_timestamp) return 0;
+
+    // uint64_t read_offset = _tinystm.addition.nv_log->read_offset;
+    // uint64_t read_block = _tinystm.addition.nv_log->read_block;
+    v_log_entry_t temp;
+    uint64_t log_length, commit_timestamp;
+
+    // read begin block and get log length
+    nv_log_get(&temp);
+    if (temp.nv_addr != BEGIN_SIG) return -1; // not the begin block
+    log_length = temp.data;
+
+    // read log and persist real data
+    for (int i = 0; i < log_length; i ++) {
+        nv_log_get(&temp);
+        *((uint64_t *)(temp.nv_addr + _tinystm.addition.base)) = temp.data;
+        pmemobj_flush(_tinystm.addition.pool, (void *)(temp.nv_addr + _tinystm.addition.base), sizeof(uint64_t));
+    }
+    
+    pmemobj_drain(_tinystm.addition.pool);
+    // read end block and persist metadata in root
+    nv_log_get(&temp);
+    if (temp.nv_addr != END_SIG) return -1; // not the begin block
+    commit_timestamp = temp.data;
+
+    struct pobj_action act[2];
+    pmemobj_set_value(_tinystm.addition.pool, &act[0], &_tinystm.addition.root->reproduce_block, _tinystm.addition.nv_log->read_block);
+    pmemobj_set_value(_tinystm.addition.pool, &act[1], &_tinystm.addition.root->reproduce_timestamp, commit_timestamp);
+    pmemobj_publish(_tinystm.addition.pool, act, 2);
+
+    return 0;
+}
+
+void nv_log_save() {
+    nv_log_recovery();
+}
+
+
+void pmem_init(char *pool_path, void (*obj_init) ()) {
+    FILE *r = fopen(pool_path, "r");
+    PMEMoid Root;
+
+    if (r == NULL) {
+        PMEMobjpool *pop = pmemobj_create(pool_path, LAYOUT_NAME, POOL_SIZE, 0666);
+        _tinystm.addition.pool = pop;
+        pmemobj_zalloc(pop, &Root, sizeof(struct root), 0);
+        _tinystm.addition.root = pmemobj_direct(Root);
+        _tinystm.addition.base = (uint64_t)_tinystm.addition.root - Root.off;
+        _tinystm.addition.nv_log = calloc(1, sizeof(nv_log_t));
+        obj_init();
+        nv_log_init();
+    }
+    else {
+        PMEMobjpool *pop = pmemobj_open(pool_path, LAYOUT_NAME);
+        _tinystm.addition.pool = pop;
+        Root = pmemobj_root(pop, sizeof(struct root));
+        _tinystm.addition.root = pmemobj_direct(Root);
+        _tinystm.addition.base = (uint64_t)_tinystm.addition.root - Root.off;
+        _tinystm.addition.nv_log = calloc(1, sizeof(nv_log_t));
+        nv_log_init();
+    }
+}
+# endif /* _LOG_H_ */
---
+++ src/page.h
@@ -0,0 +1,207 @@
+# ifndef _PAGE_H_
+# define _PAGE_H_
+
+# include "stm_internal.h"
+
+# define PAGE_LENGTH    12
+# define PAGE_SIZE      (1 << PAGE_LENGTH)                  // 4K
+# define NVM_LENGTH     30
+# define DRAM_LENGTH    30
+# define VPN_NUM        (1 << (NVM_LENGTH - PAGE_LENGTH))   // 64k
+# define PPN_LENGTH     (DRAM_LENGTH - PAGE_LENGTH)         // 18
+# define VPN_LENGTH     (NVM_LENGTH - PAGE_LENGTH)          // 18
+# define PPN_NUM        (1 << (DRAM_LENGTH - PAGE_LENGTH))  // 64k
+
+typedef union v_page_inf {
+    struct {
+        uint64_t vaild : 1;
+        uint64_t used : 63; // thread used bitmap
+    };
+    uint64_t v_page_inf;
+} v_page_inf_t;
+
+typedef struct free_page_entry {
+    struct free_page_entry *pre;
+    struct free_page_entry *next;
+    volatile v_page_inf_t page_inf;        
+    uint64_t PPN;
+    uint64_t VPN;
+} free_page_entry_t;
+
+struct free_page_head {
+    uint32_t free_num;
+    pthread_spinlock_t lock;
+    free_page_entry_t *head;
+    free_page_entry_t *tail;
+} free_page_head;
+
+typedef struct page_entry {
+    uint64_t touch_id;
+    free_page_entry_t *free_page;
+} page_entry_t;
+
+
+void page_init();
+uint64_t *page_use(stm_tx_t *tx, uint64_t nv_addr);
+void page_free(stm_tx_t *tx, uint64_t nv_addr, uint64_t commit_timestamp);
+
+# include "stm_internal.h"
+// global
+page_entry_t page_table[VPN_NUM];
+
+static inline uint64_t v_page_alloc() {
+    return (uint64_t)aligned_alloc(PAGE_SIZE, PAGE_SIZE) >> PAGE_LENGTH;
+}
+
+static inline uint64_t *addr_nv_2_v(uint64_t PPN, uint64_t nv_addr) {
+    return (uint64_t *)((PPN << PAGE_LENGTH) | ((PAGE_SIZE - 1) & nv_addr));
+}
+
+// move a node from tail to head
+static inline void free_page_roll() {
+    free_page_entry_t * node = free_page_head.tail;
+    free_page_head.tail = node->pre;
+    node->pre = NULL;
+    free_page_head.tail->next = NULL;
+    free_page_head.head->pre = node;
+    node->next = free_page_head.head;
+    free_page_head.head = node->next;
+}
+
+static int page_map_(stm_tx_t *tx, uint64_t nv_addr) {
+    pthread_spin_lock(&free_page_head.lock);
+
+    // check if other thread has mapped the nv_page
+    uint64_t VPN = nv_addr >> PAGE_LENGTH;
+    if (page_table[VPN].free_page != NULL && page_table[VPN].free_page->VPN == VPN && page_table[VPN].free_page->page_inf.vaild) {
+        pthread_spin_unlock(&free_page_head.lock);
+        return 0;
+    }
+    
+    // check if touchid is bigger than reproduce timestamp
+    if (page_table[VPN].touch_id > _tinystm.addition.root->reproduce_timestamp) {
+        pthread_spin_unlock(&free_page_head.lock);
+        return -1;
+    }
+
+    v_page_inf_t old_v, new_v;
+    
+    while (1) {
+        old_v = free_page_head.tail->page_inf;
+        new_v.v_page_inf = 0;
+
+        // v_page in used
+        if (old_v.used != 0 || ATOMIC_CAS_FULL(&free_page_head.tail->page_inf.v_page_inf, old_v.v_page_inf, new_v.v_page_inf) == 0) {
+            free_page_roll();
+            continue;
+        }
+        
+        // not used but mapped, clean the old page_table entry
+        if (old_v.vaild == 1) {
+            page_table[free_page_head.tail->VPN].free_page = NULL;
+        }
+
+        // map to new nv_page 
+        free_page_head.tail->VPN = VPN;
+        new_v.vaild = 1;
+        new_v.used = 1 << tx->addition.thread_nb;
+        free_page_head.tail->page_inf = new_v;
+
+        free_page_roll();
+        break;
+    }
+    
+    pthread_spin_unlock(&free_page_head.lock);
+    return 0;
+}
+
+static void page_map(stm_tx_t *tx, uint64_t nv_addr) {
+    while (page_map_(tx,nv_addr) < 0) {
+        nv_log_reproduce();
+    }
+}
+
+void page_init() {
+    free_page_entry_t *node;
+
+    free_page_head.free_num = PPN_NUM;
+    pthread_spin_init(&free_page_head.lock, 0);
+    free_page_head.head = malloc(sizeof(free_page_entry_t));
+    free_page_head.head->PPN = v_page_alloc();
+    free_page_head.head->VPN = 0;
+    free_page_head.head->page_inf.v_page_inf = 0;
+    free_page_head.head->pre = 0;
+
+    node = free_page_head.head;
+    for (int i = 0; i < PPN_NUM - 1; i++) {
+        node->next = malloc(sizeof(free_page_entry_t));
+        node->next->PPN = v_page_alloc();
+        node->next->VPN = 0;
+        node->next->page_inf.v_page_inf = 0;
+        node->next->pre = node;
+        node = node->next;
+    }
+    node->next = NULL;
+
+    free_page_head.tail = node;
+}
+
+// map the page and set write set; if page has mapped, return directly
+uint64_t *page_use(stm_tx_t *tx, uint64_t nv_addr) {
+    uint64_t VPN = nv_addr >> PAGE_LENGTH;
+    free_page_entry_t *page_entry = page_table[VPN].free_page;
+    v_page_inf_t old_v, new_v, fail_v;
+    
+    // nv_page not mapped to v_page
+    if (page_entry == NULL || page_entry->page_inf.vaild == 0) {
+        page_map(tx,nv_addr);
+        return addr_nv_2_v(page_entry->PPN, nv_addr);
+    }
+        
+    // in write set, return directly
+    old_v = page_entry->page_inf;
+    if ((old_v.used & (1 << tx->addition.thread_nb)) != 0) return addr_nv_2_v(page_entry->PPN, nv_addr);
+
+    new_v = page_entry->page_inf;
+    new_v.used |= 1 << tx->addition.thread_nb;
+
+    // CAS fail
+    while (ATOMIC_CAS_FULL(&page_entry->page_inf.v_page_inf, old_v.v_page_inf, new_v.v_page_inf) == 0){
+        fail_v = page_entry->page_inf;
+        
+        // page unmapped
+        if (fail_v.vaild == 0) {
+            page_map(tx,nv_addr);
+            return addr_nv_2_v(page_entry->PPN, nv_addr);
+        }
+
+        // other tx has changed the write set
+        old_v = page_entry->page_inf;
+        new_v = page_entry->page_inf;
+        new_v.used |= 1 << tx->addition.thread_nb;
+    }
+
+    return addr_nv_2_v(page_entry->PPN, nv_addr);
+}
+
+// remove tx from write set when tx committed or abort and update touvh id
+void page_free(stm_tx_t *tx, uint64_t nv_addr, uint64_t commit_timestamp) {
+    uint64_t VPN = nv_addr >> PAGE_LENGTH;
+    free_page_entry_t *page_entry = page_table[VPN].free_page;
+    v_page_inf_t old_v, new_v;
+    uint64_t new_timestamp = commit_timestamp, old_timestamp;
+
+    // CAS modify write set
+    do {
+        old_v = page_entry->page_inf;
+        if ((old_v.used & (1 << tx->addition.thread_nb)) != 0) break;
+        new_v = page_entry->page_inf;
+        new_v.used &= ~(1 << tx->addition.thread_nb);
+    } while (ATOMIC_CAS_FULL(&page_entry->page_inf.v_page_inf, old_v.v_page_inf, new_v.v_page_inf) == 0);
+
+    do {
+        old_timestamp = page_table[VPN].touch_id;
+        if (new_timestamp <= old_timestamp || new_timestamp == 0) break;
+    } while (ATOMIC_CAS_FULL(&page_table[VPN].touch_id, old_timestamp, new_timestamp) == 0);
+}
+# endif /* _PAGE_H_ */
